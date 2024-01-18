71d5c17532e197ebbf8555d112601b86b2324661
Renaud Bresson <renaud.cci@gmail.com>
2024-01-18 17:02:20 +0100

    create/first-step-finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function Square({ value }: { value: any }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
+  return (
+    <>
+    <div className="board-row">
+      <Square value="1" />
+      <Square value="2" />
+      <Square value="3" />
+    </div>
+    <div className="board-row">
+      <Square value="4" />
+      <Square value="5" />
+      <Square value="6" />
+    </div>
+    <div className="board-row">
+      <Square value="7" />
+      <Square value="8" />
+      <Square value="9" />
+    </div>
+  </>
+  );
 }
