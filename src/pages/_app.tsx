565903e6b9f4f20baae1bc9a8c32f0d843ed1698
Renaud Bresson <renaud.cci@gmail.com>
2024-01-23 10:57:54 +0100

    End-tutorial-before-improvements-ideas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,18 +14,53 @@ function Square({ value , onSquareClick }: { value: string , onSquareClick: () =
   );
 }
 
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+interface BoardProps {
+  xIsNext: boolean;
+  squares: string[];
+  onPlay: (squares: string[]) => void;
+}
+
+function Board({ xIsNext, squares, onPlay }: BoardProps) {
+
+  const winner = calculateWinner(squares);
+  let status = winner ? `${winner} a gagné` : `Prochain tour : ${xIsNext ? "X" : "O"}`;
 
   function handleClick(i : number) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
+    // Copier un tableau plutot que de le mofifier directement pour respecter l'immutabilité
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
-    setSquares(nextSquares);
+
+    nextSquares[i] = xIsNext ? "X" : "O";
+
+    onPlay(nextSquares);
   }
 
   return (
     <>
+    <div className="status">{status}</div>
     <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -42,5 +77,43 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
   </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
+  function handlePlay(nextSquares : string[]) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove : number) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    description = move > 0 ? 'Aller au coup #' + move : 'Revenir au début';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
